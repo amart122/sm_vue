394d48d5ef69a13f6434ead754a67acd42c33c88
Aram Martinez <amart122@asu.edu>
2021-09-01 21:47:17 -0700

    Introduced the Main component (Dashboard)

---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="main-app-container" class="">
+    <MainStage></MainStage>
+    <SideBar></SideBar>
+  </div>
+</template>
+
+<script>
+import MainStage from "./MainStage.vue";
+import SideBar from "./SideBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainStage,
+    SideBar,
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      if (!vm.$store.getters["getCurrentUser"]) {
+        vm.$router.push({ path: "/account/login" });
+      }
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+@import "src/assets/scss/sm_global.scss";
+</style>
